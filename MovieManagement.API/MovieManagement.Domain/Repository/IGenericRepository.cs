66260b13a9eec3af803a47503abd143e72e58812
alex-toy <alexei.80@hotmail.fr>
2023-11-25 08:29:30 +0100

    generic repository

--- MovieManagement.API/MovieManagement.Domain/Repository/IGenericRepository.cs
+++ MovieManagement.API/MovieManagement.Domain/Repository/IGenericRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace MovieManagement.Domain.Repository
+﻿using System.Linq.Expressions;
+
+namespace MovieManagement.Domain.Repository
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
     }
 }
